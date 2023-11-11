5b8b382c064327ef5350a6f1cfc4645cc2ad43e0
LitchiCheng <974782852@qq.com>
2023-11-11 17:38:41 +0800

    feat:增加马尔科夫链示例

---
+++ Markvo-Decision-Process.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# 定义转移矩阵
+P = np.array([[0.9, 0.1],
+              [0.5, 0.5]])
+
+# 初始状态
+state = np.array([1, 0])
+
+# 迭代次数
+n_iterations = 10
+
+# 马尔科夫链迭代
+for _ in range(n_iterations):
+    state = np.dot(state, P)
+    print(state)
